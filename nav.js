d1b788dced49920d155122151e4f9f7ba1c41ecc
Mert Akengin <mert@n0pe.me>
2016-10-18 09:53:42 +0300

    team htm added

---
+++ nav.js
@@ -0,0 +1,20 @@
+//
+// nav.js
+// TEDxBahcesehir - 2017
+//
+
+function _menus() {
+	var __elements = document.querySelectorAll("[collapsed]");
+	for (var i=0;i<__elements.length;i++) {
+		__elements[i].onclick = (function(e) {
+			if(e.target != this)
+				return;
+			if(this.attributes.getNamedItem("collapsed"))
+				this.removeAttribute("collapsed");
+			else
+				this.setAttribute("collapsed","");
+			return;
+		});
+	}
+};
+_menus();
