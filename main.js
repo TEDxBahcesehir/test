dec534521d118a8b56a68e3434934a08aee46f54
Mert Akengin <mert@n0pe.me>
2016-10-27 15:54:48 +0300

    workaround cloudflare minifier #2

--- main.js
+++ main.js
@@ -66,11 +66,11 @@ window.onscroll = (function(e) {
 function menuToggle() {
 	var nav = document.getElementsByTagName("nav")[0];
 	var main = document.getElementById("main");
-	document.body.style.overflow = (nav.style.left == 0)?"hidden":"";
-	//nav.style.width = ((nav.style.width) == 0)?"280px":"";
-	nav.style.left = (nav.style.left == "")?"0px":"";
-	main.style.marginLeft = ((main.style.marginLeft) == 0)?"280px":"";
+	document.body.style.overflow = (nav.style.left == 0)?"hidden":null;
+	//nav.style.width = ((nav.style.width) == 0)?"280px":null;
+	nav.style.left = (!nav.style.left)?"0px":null;
+	main.style.marginLeft = ((main.style.marginLeft) == 0)?"280px":null;
 	var menu = document.getElementsByTagName("nav")[1];
-	menu.style.left = (menu.style.left == 0)?"216px":"";
+	menu.style.left = (menu.style.left == 0)?"216px":null;
 	return;
 }
